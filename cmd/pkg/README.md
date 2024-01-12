7d2ad11a7fb2878618093607f47c8685010047ff
Rett Berg <vitiral@gmail.com>
2024-01-12 12:47:13 -0600

    initial pkg.lua impl

---
+++ cmd/pkg/README.md
@@ -0,0 +1,57 @@
+# pkg: better lua package creation and importing
+
+Lua pkg improves lua's module management over the build-in `require` function
+while also acting as a command which generates `name-version.rockspec` files.
+
+How to import packages:
+* Add the path to `pkg.lua` on your `LUA_PATH`
+* Put the directories containing your lua pks somewhere on `LUA_PKGS`
+  environment variable (`;` separated)
+* ```
+  local pkg = require'pkg'
+  local myLib = pkg'myLib'
+  ```
+
+How to create packages:
+* Add a `PKG.lua` in your library's root.
+* Define global variables for the required fields:
+  * `name`: the name of your package (`package` in [rockspec])
+  * `version`: [rockspec] version string, i.e `"0.1-5"`
+  * `url`: url to the source code, i.e. `"git+http://github.com/civboot/civlua"`
+  * `srcs`: source files to include relative to pkgs, either in path form (`mod.lua`)
+    or in keyval form (`mod = 'path/to/mod.lua'`).
+  * `dirs`: causes pkg search to continue in the given sub-directories.
+    Can be used to construct trees of packages.
+  * `rockspec`: (optional) provide starting rockspec when generating it
+
+Example:
+
+```
+name    = 'myLib'
+version = '0.1-5'
+url     = 'git+http://github.com/civboot/myLib'
+srcs    = {
+    'myLib.lua',      -- name: myLib
+    'myLib/sub1.lua', -- name: mylib.sub1
+    ['myLib.sub2'] = 'lib/myLib/submodule.lua'},
+}
+```
+
+[rockspec]: https://github.com/luarocks/luarocks/wiki/Rockspec-format
+
+## Why?
+Because `LUA_PATH` is clumsy and annoying.
+Luarocks is awesome, but it doens't solve local path management. Also,
+`rockspec` is very opinionated on file names and I wanted a way to auto
+generate the `.rockspec` files.
+
+I am personally using this library to maintain 10+ projects at
+[civlua](http://github.com/civboot/civlua) and will be making a `pkgrock`
+cmd utility to help me.
+
+## How?
+`PKG.lua` files are executed in a sandbox. Their environment has access to only:
+`string table select pairs ipairs next error assert
+math.abs math.ceil math.foor math.max math.min math.maxinteger math.tonumber`
+
+The globals they create are then read as the configuration.
---
+++ cmd/pkg/pkg.lua
@@ -0,0 +1,141 @@
+-- pkg: better lua pkg creation and importing
+
+local push = table.insert
+local M = {}
+
+M.PKGS = {} -- loaded pkgs
+
+-- These are modified before loading the package.
+-- The package can inspect it to (for example) know it's version string,
+-- i.e. local version = require'pkg'.PKG.version
+--
+-- It is recommended to do this before executing any other `pkg()` calls!
+M.PKG  = nil -- PKG.lua being loaded
+M.PATH = nil -- path to lua file being loaded
+
+M.CHUNK_SIZE = 4096
+
+-- reduced math (no randomness)
+M.MATH = {
+  abs=math.abs, ceil=math.ceil, floor=math.floor,
+  max=math.max, min=math.min,
+  maxinteger=math.maxinteger, tonumber=tonumber,
+}
+M.math = setmetatable(M.MATH, {
+  __index   = function(self, i) return M.MATH[i] end,
+  __newindex= function() error'cannot modify math' end,
+})
+
+
+M.ENV = {
+  string=string, table=table, utf8=utf8,
+  type=type,   select=select,
+  pairs=pairs, ipairs=ipairs, next=next,
+  error=error, assert=assert,
+  math=M.math,
+}
+
+M.createEnv = function(env, new)
+  new = new or {}
+  new.__index = function(e, i) return env[i] end
+  return setmetatable(new, env)
+end
+
+M.callerSource = function()
+  local info = debug.getinfo(3)
+  return string.format('%s:%s', info.source, info.currentline)
+end
+
+-- loadraw(chunk, env) -> ok, result
+M.loadraw = function(chunk, env, name)
+  name = name or M.callerSource()
+  local res = setmetatable({}, env)
+  local e, err = load(chunk, path, 'bt', res)
+  if err then return false, err end
+  e()
+  return true, setmetatable(res, nil)
+end
+
+-- load a path with env metatable M.ENV
+M.load = function(path, env)
+  local f = io.open(path)
+  local function chunk() return f:read(M.CHUNK_SIZE) end
+  local ok, res = M.loadraw(chunk, M.createEnv(M.ENV, env or {}), path)
+  f:close(); return ok, res
+end
+
+M.split = function(pathstr) return pathstr:gmatch'[^;]+' end
+
+--------------------
+-- Working with file paths
+M.path = {}
+
+-- join a table of path components
+M.path.concat = function(t)
+  if #t == 0 then return '' end
+  local root = (t[1]:sub(1,1)=='/') and '/' or ''
+  local dir  = (t[#t]:sub(-1)=='/') and '/' or ''
+  local out = {}
+  for i, p in ipairs(t) do
+    p = string.match(p, '^/*(.-)/*$')
+    if p ~= '' then add(out, p) end
+  end; return root..table.concat(out, '/')..dir
+end
+
+
+M.path.first = function(path)
+  if path:sub(1,1) == '/' then return '/', path:sub(2) end
+  local a, b = path:match('^(.-)/(.*)$')
+  if not a or a == '' or b == '' then return path, '' end
+  return a, b
+end
+
+M.path.last = function(path)
+  local a, b = path:match('^(.*)/(.+)$')
+  if not a or a == '' or b == '' then return '', path end
+  return a, b
+end
+
+-- return whether a path has any '..' components
+M.path.hasBacktrack = function(path)
+  return string.match'^%.%.$' or string.match'^%.%./'
+      or string.match'/%.%./' or string.match'/%.%.$'
+end
+
+-- recursively find a package in dirs
+M.findpkg = function(base, dirs, name)
+  local pat = '^'..name..'%.'
+  for dir in ipairs(dirs) do
+    local pkgdir = M.path.concat{base, dir}
+    local path = M.path.concat{pkgdir, 'PKG.lua'}
+    local ok, pkg = M.load(path); if not ok then error(pkg) end
+    if (pkg.name == name) or pkg.name:match(pat) then return pkgdir, pkg end
+    if pkg.dirs then
+      local subdir, subpkg = M.findpkg(pkgdir, pkg.dirs, name)
+      if subpkg then return subdir, subpkg end
+    end
+  end
+end
+
+-- load the pkg
+M.__call = function(name)
+  if M.PKGS[name] then return M.PKGS[name] end
+  local dirs = {}; for _, d in M.split(os.getenv'LUA_PKGS') do push(dirs, d) end
+  local pkgdir, pkg = M.findpkg('', dirs, name)
+  if not pkg then error('PKG '..name..' not found') end
+  for k, v in ipairs(pkg.srcs) do
+    local mpath, mname; if type(k) == 'string' then
+      mpath, mname = v, k
+    elseif type(k) == 'number' then
+      mpath, mname = v, v:gsub('.%lua$', ''):gsub('/', '.')
+    else error('invalid srcs key: '..tostring(k)) end
+    if mname == name then
+      M.PKG, M.PATH = pkg, path.concat(pkgdir, mpath)
+      M.PKGS[mname] = dofile(M.PATH)
+      return M.PKGS[mname]
+    end
+  end
+  error('PKG found but not sub-module: '..name)
+end
+
+return M
