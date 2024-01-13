a4332eb952fac6babaf46e18b7efe8636844fce9
Rett Berg <vitiral@gmail.com>
2024-01-13 10:15:06 -0600

    add pkg/PKG.lua

---
+++ lib/pkg/PKG.lua
@@ -0,0 +1,13 @@
+-- Note: this is only for demonstration and working with the pkgrock utility.
+-- Obviously you cannot import pkg using pkg (you must require'pkg')
+
+name     = 'pkg'
+version  = '0.1-2'
+url      = 'git+http://github.com/civboot/civlua'
+summary  = "better lua package creation and importing"
+homepage = "https://github.com/civboot/civlua/blob/main/lib/pkg/README.md"
+license  = "UNLICENSE"
+srcs = { 'pkg.lua' }
+deps = {
+  "lua ~> 5.3",
+}
