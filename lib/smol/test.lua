beaeb4c46e86833171ee1d4c06e2c148a8c2f290
Rett Berg <vitiral@gmail.com>
2024-10-11 16:00:28 -0500

    smol run works

--- lib/smol/test.lua
+++ lib/smol/test.lua
@@ -3,5 +3,5 @@ local T = require'civtest'.Test
 local smol = require'smol'
 
 T.rdecode = function()
-  T.eq('zzz', smol.rdecode('\x43z'))
+  T.eq('zzz', smol.rdecode('\x03\x43z')) -- len=3 RUN(3, 'z')
 end
