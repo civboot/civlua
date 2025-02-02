64a3d226977f64a4363128edc296ff26bbf1571c
Rett Berg <vitiral@gmail.com>
2025-02-02 09:34:21 -0700

    improve string.concat

--- lib/ds/ds.c
+++ lib/ds/ds.c
@@ -24,14 +24,19 @@ typedef lua_State LS;
 // concat(sep, ...) --> string
 // concatenate all values (calling tostring on them) separated by sep.
 static inline int l_concat(LS* L) {
+  size_t slen; uint8_t const* sep = luaL_checklstring(L, 1, &slen);
   int lasti = lua_gettop(L);
   if(lasti == 1) { lua_pushstring(L, ""); return 1; }
-  size_t slen; uint8_t const* sep = luaL_checklstring(L, 1, &slen);
-  luaL_Buffer lb; luaL_buffinit(L, &lb);
-  luaL_tolstring(L, 2, NULL); luaL_addvalue(&lb);
-  for(int i=3; i <= lasti; i++) {
+  int size = slen * (lasti - 2);  // size of all separators
+  for(int i=2; i <= lasti; i++) { // convert tostring and calc bufsize
+    luaL_tolstring(L, i, NULL); size += lua_rawlen(L, -1);
+  }
+  luaL_Buffer lb; luaL_buffinitsize(L, &lb, size);
+  size_t alen; uint8_t const* arg;
+  arg = lua_tolstring(L, lasti+1, &alen); luaL_addlstring(&lb, arg, alen);
+  for(int i = lasti+2; i <= lasti + (lasti - 1); i++) {
     luaL_addlstring(&lb, sep, slen);
-    luaL_tolstring(L, i, NULL); luaL_addvalue(&lb);
+    arg = lua_tolstring(L, i, &alen); luaL_addlstring(&lb, arg, alen);
   }
   luaL_pushresult(&lb);
   return 1;
