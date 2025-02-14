c2b6c00501b2ecf8796e2e82c9b897fe04376ad0
Rett Berg <vitiral@gmail.com>
2025-02-14 14:14:49 -0700

    patch 1 works

--- cmd/pvc/testdata/notes.sh
+++ cmd/pvc/testdata/notes.sh
@@ -1,4 +1,3 @@
-CREATED_LABEL='(created)	1969-12-31 17:00:00.000000000 -0700'
 DELETED_LABEL='(deleted)	1969-12-31 17:00:00.000000000 -0700'
 
 D=$PWD
@@ -10,9 +9,9 @@ function create1() {
   echo "cd $TD"; cd $TD
   echo ls;       ls
   echo creating 1
-  diff -N --unified=1 DNE story.txt.1 --label="$CREATED_LABEL" --label=story.txt \
+  diff -N --unified=1 DNE story.txt.1 --label=story.txt --label=story.txt \
     > patch.story.txt.1
-  diff -N --unified=1 DNE hello.lua.1 --label="$CREATED_LABEL" --label=hello.lua \
+  diff -N --unified=1 DNE hello.lua.1 --label=hello.lua --label=hello.lua \
     > patch.hello.lua.1
   cat patch.story.txt.1 patch.hello.lua.1 > patch.1
 }
@@ -20,20 +19,26 @@ function create1() {
 function create2() {
   cd $TD
   # creating 2 (with deleted hello.lua)
-  diff -N --unified=1 story.1 story.2 --label=story.txt --label=story.txt \
-    > patch.story.2 
-  diff -N --unified=1 hello.1 DNE  --label=hello.lua  \
-    --label=$DELETED_LABEL > patch.hello.2
-  cat patch.story.2 patch.hello.2 > patch.2
+  diff -N --unified=1 story.txt.1 story.txt.2 --label=story.txt --label=story.txt \
+    > patch.story.txt.2
+  diff -N --unified=1 hello.lua.1 DNE  --label=hello.lua  \
+    --label="$DELETED_LABEL" > patch.hello.lua.2
+  cat patch.story.txt.2 patch.hello.lua.2 > patch.2
 }
+
+function efile() { echo "## $1"; cat "$1"; }
 
 function patch1() {
   rm -rf $OD; mkdir $OD; cd $OD
   cat $TD/patch.1 | patch -u
+  efile $OD/story.txt
+  efile $OD/hello.lua
 }
 
 function patch2() {
   cat $TD/patch.2 | patch -u
+  efile $OD/story.txt
+  efile $OD/hello.lua
 }
 
 echo "Running $1"
