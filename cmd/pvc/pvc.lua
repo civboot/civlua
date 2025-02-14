35670fc65a14dd67f2ca71673f42287a5a12c844
Rett Berg <vitiral@gmail.com>
2025-02-13 17:00:51 -0700

    path

--- cmd/pvc/pvc.lua
+++ cmd/pvc/pvc.lua
@@ -17,27 +17,34 @@ M.Patch = mty'Patches' {
   'branch [string]: path to the branch',
   'snap [string]: path to patch snapshot (if exists)',
   'minId [int]', 'maxId [int]',
-  'depth [int]: length of all change directories',
+  '_depth [int]: length of all change directories',
 }
+
+--- get or set depth (asserting it's valid)
+M.Patch.depth = function(pch, d)
+  if not d then return pch._depth end
+  assert(d % 2 == 0); assert(d > 0); pch._depth = d
+  return pch
+end
 
 --- given a patch id return it's common (non-merged) path
 --- relative to [$branch/patches/]
 M.Patch.patchPath = function(pch, id) --> path
   local dirstr = tostring(id)
-  assertf(#dirstr <= pch.depth,
-    '%i has longer length than depth=%i', id, pch.depth)
-  dirstr = srep('0', pch.depth - #dirstr)..dirstr -- zero padded
-  local path = {}; for i=1,#dirstr-3,2 do
+  assertf(#dirstr <= pch._depth,
+    '%i has longer length than depth=%i', id, pch._depth)
+  dirstr = srep('0', pch._depth - #dirstr)..dirstr -- zero padded
+  local path = {}; for i=1,#dirstr-2,2 do
     push(path, dirstr:sub(i,i+1)) -- i.e. 00/12.p
   end
   push(path, id..'.p')
   return pth.concat(path)
 end
 
-M.Patch.__call = function(pch)
-  local nextid = pch.id + 1
-  local nextfile = tostring(nextid);
-  nextfile = srep('0', pch.depth - #nextfile)..nextfile
+--- Get next (id, path). Mutates id so it can be used as an iterator.
+M.Patch.__call = function(pch) --> id, path
+  local id = pch.id; if id > pch.maxId then return end
+  pch.id = id + 1; return id, pch:patchPath(id)
 end
 
 return M
