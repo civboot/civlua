799ed36cab3354ae27a9c2bda9a1b07adcad67b6
Rett Berg <vitiral@gmail.com>
2025-08-21 07:55:27 -0600

    add overlay, not used yet. Fix isTruncate issue

--- lib/lines/lines/futils.lua
+++ lib/lines/lines/futils.lua
@@ -8,13 +8,11 @@ local U3File = require'lines.U3File'
 local pth = require'ds.path'
 local trace = require'ds.log'.trace
 
-local TRUNC = {w=true, ['w+']=true}
-
 --- load or reindex the file at path to/from idxpath.
 M.loadIdx = function(f, idxpath, fmode, reindex) --> idxFile
   trace('loadIdx idxpath=%s mode=%s reindex=%q', idxpath, fmode, reindex)
   local fstat, xstat = assert(ix.stat(f))
-  if TRUNC[fmode] then goto createnew end
+  if fd.isTrunc(fmode) then goto createnew end
   xstat = ix.stat(idxpath)
   if xstat and fd.modifiedEq(fstat, xstat) then
     return U3File:load(idxpath)
