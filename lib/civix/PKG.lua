f10a9eb6e684453c42001888bef2949f1e4743e7
Rett Berg <vitiral@gmail.com>
2025-11-22 16:30:50 -0700

    civix PKG

--- lib/civix/PKG.lua
+++ lib/civix/PKG.lua
@@ -1,19 +1,14 @@
-name     = 'civix'
-summary  = "Unix sys library"
-homepage = "https://lua.civboot.org#Package_civix"
-license  = "UNLICENSE"
-version  = '0.1-8'
-url      = 'git+http://github.com/civboot/civlua'
-doc      = 'README.cxt'
-srcs = {
-  'civix.lua',
-  ['civix.lib'] = 'civix.c',
-  'civix/testing.lua',
+summary"Unix sys library"
+local P = pkg {
+  name     = 'civix',
+  homepage = "https://lua.civboot.org#Package_civix",
+  license  = "UNLICENSE",
+  version  = '0.1-8',
+  url      = 'git+http://github.com/civboot/civlua',
+  doc      = 'README.cxt',
 }
-libs = {
-  ['civix.lib'] = 'civix.so',
-}
-deps = {
+
+import {
   "lua ~> 5.3",
   "shim   ~> 0.1",
   "fd     ~> 0.1",
@@ -21,3 +16,16 @@ deps = {
   "ds     ~> 0.1",
   "lap    ~> 0.1",
 }
+
+P.lua = lua {
+  src = {
+    'civix.lua',
+    ['civix.lib'] = 'civix.c',
+    'civix/testing.lua',
+  },
+  lib = {
+    ['civix.lib'] = 'civix.so',
+  },
+}
+
+return P
