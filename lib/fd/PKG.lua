249aa7f958deaa2aed896882769f1614fb52ead9
Rett Berg <vitiral@gmail.com>
2025-11-22 16:40:04 -0700

    continue

--- lib/fd/PKG.lua
+++ lib/fd/PKG.lua
@@ -1,19 +1,26 @@
-name     = 'fd'
-summary  = "filedescriptor interfaces"
-homepage = "https://lua.civboot.org#Package_fd"
-license  = "UNLICENSE"
-version  = '0.1-7'
-url      = 'git+http://github.com/civboot/civlua'
-doc = 'README.cxt'
-srcs = {
-  'fd.lua',
-  ['fd.sys'] = 'fd.c',
-  'fd/IFile.lua',
-}
-libs = {
-  ['fd.sys'] = 'fd.so',
-}
-deps = {
+summary"filedescriptor interfaces"
+import {
   "lua ~> 5.3",
   "metaty ~> 0.1",
 }
+local P = pkg {
+  name     = 'fd',
+  homepage = "https://lua.civboot.org#Package_fd",
+  license  = "UNLICENSE",
+  version  = '0.1-7',
+  url      = 'git+http://github.com/civboot/civlua',
+  doc      = 'README.cxt',
+}
+
+P.lua = lua {
+  src = {
+    'fd.lua',
+    ['fd.sys'] = 'fd.c',
+    'fd/IFile.lua',
+  },
+  lib = {
+    ['fd.sys'] = 'fd.so',
+  },
+}
+
+return P
